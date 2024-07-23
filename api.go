bee40b836de73004143a6f1c92b76782529db408
pandeptwidyaop <widya.oktapratama@gmail.com>
2024-07-23 17:07:46 +0800

    feat: implement stringer interface and add mit license

--- api.go
+++ api.go
@@ -1,14 +1,16 @@
 package api
 
-type ResponseBag struct {
+import "encoding/json"
+
+type responseBag struct {
 	HttpStatus  int         `json:"status"`
 	HttpData    interface{} `json:"data"`
 	HttpMessage interface{} `json:"message"`
 	HttpMeta    interface{} `json:"meta"`
 }
 
-func New() *ResponseBag {
-	return &ResponseBag{
+func New() *responseBag {
+	return &responseBag{
 		HttpStatus:  200,
 		HttpData:    nil,
 		HttpMessage: nil,
@@ -16,22 +18,30 @@ func New() *ResponseBag {
 	}
 }
 
-func (r *ResponseBag) Status(status int) *ResponseBag {
+func (r *responseBag) Status(status int) *responseBag {
 	r.HttpStatus = status
 	return r
 }
 
-func (r *ResponseBag) Data(data interface{}) *ResponseBag {
+func (r *responseBag) Data(data interface{}) *responseBag {
 	r.HttpData = data
 	return r
 }
 
-func (r *ResponseBag) Message(message interface{}) *ResponseBag {
+func (r *responseBag) Message(message interface{}) *responseBag {
 	r.HttpMessage = message
 	return r
 }
 
-func (r *ResponseBag) Meta(meta interface{}) *ResponseBag {
+func (r *responseBag) Meta(meta interface{}) *responseBag {
 	r.HttpMeta = meta
 	return r
+}
+
+func (r *responseBag) String() string {
+	b, err := json.Marshal(r)
+	if err != nil {
+		return ""
+	}
+	return string(b)
 }
